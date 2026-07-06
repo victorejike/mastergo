754686647bfc847754700412dd6b5e3562e1374e
victorejike <victorejike@gmail.com>
2026-07-06 11:04:58 +0100

    printcomb2

--- quest1f/main.go
+++ quest1f/main.go
@@ -2,15 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
+func PrintComb2() {
 	for i := 0; i <= 98; i++ {
 		for j := i + 1; j <= 99; j++ {
-			fmt.Printf("%d%d ", i, j)
+			fmt.Printf("%02d %02d", i, j)
 
+			
 			if !(i == 98 && j == 99) {
 				fmt.Print(", ")
 			}
 		}
 	}
+	// Print a newline at the very end
 	fmt.Println()
+}
+
+func main() {
+	PrintComb2()
 }
